47ef7ae16cd673505fb91c293e681394f3db6c82
cwj627 <cwj627@gmail.com>
2019-05-06 17:45:49 -0700

    adding php calculator

---
+++ A4/add.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<h1> Addition Calculator </h1>
+<form action="addresult.php" method="get"> 
+	<input type="text" name="n1">
+	+
+	<input type="text" name="n2">
+	<input type="submit" value="submit">
+</form>
+
+
+</body>
+</html>
---
+++ A4/addresult.php
@@ -0,0 +1,5 @@
+<?php
+$result = (double)$_GET["n1"]+(double)$_GET["n2"];
+print "result is: ". $result;
+//GET become available when we use the method in add.php
+?>

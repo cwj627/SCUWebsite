7781b70dde04ad2c9aa79964de432d94ac2fd7f4
cwj627 <cwj627@gmail.com>
2019-05-12 19:55:20 -0700

    adding main.php

---
+++ A4/main.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Choose your favorite movie</title>
+</head>
+<body>
+	<form action="movie.php" method="get">
+		<select name="film">
+			<option value="fightclub">Fight Club</option>
+			<option value="princessbride">Princess Bride</option>
+			<option value="mortalkombat">Mortal Kombat</option>
+			<option value="tmnt">TMNT</option>
+			<option value="tmnt2">TMNT2</option>
+
+		</select>
+		<input type="Submit">
+	</form>
+
+</body>
+</html>
